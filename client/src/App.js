58d3044ecca4aa05f16da0d6993cab0032e5ef39
hannanel100 <hannanel100@gmail.com>
2021-01-03 01:18:52 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form/Form'
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,30 @@
+import React, { useState, useRef } from 'react'
+
+const Form = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false)
+    const [selectedFile, setSelectedFile] = useState('');
+
+    const fileInputRef = useRef(null);
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setIsSubmitted(true);
+        console.log(selectedFile);
+        //TODO: add http to post selected file to backend
+    }
+    const fileInputHandler = (e) => {
+        setSelectedFile(e.target.files[0])
+    }
+    
+    return (
+        <form onSubmit={(e) => submitHandler(e)}>
+            <label htmlFor="img">Select image:</label>
+            <input type="file" id="img" name="img" accept="image/*"
+                onChange={(e) => fileInputHandler(e)} />
+            <input type="submit" />
+        </form>
+    )
+}
+
+export default Form
+
+
