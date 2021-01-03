f4179f479422a874364703d35920b3b04bf02414
hannanel gershinsky <hannanel100@gmail.com>
2021-01-03 20:02:21 +0200

    upload to client storage working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import Form from "./components/Form/Form";
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Form />
-    </div>
-  );
-}
+const App = () => (
+  <div className="container mt-4">
+    <h3 className="display-4 text-center mb-4">File Uploader</h3>
+    <Form />
+  </div>
+);
 
 export default App;
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,32 +1,69 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Form = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [selectedFile, setSelectedFile] = useState("");
+  const [file, setFile] = useState();
+  const [fileName, setFilename] = useState("Choose File");
+  const [uploadedFile, setUploadedFile] = useState({});
 
-  const fileInputRef = useRef(null);
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     setIsSubmitted(true);
-    console.log(selectedFile);
-    //TODO: add http to post selected file to backend
+    const formData = new FormData();
+
+    formData.append("file", file);
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      const { fileName, filePath } = res.data;
+      setUploadedFile({ fileName, filePath });
+    } catch (error) {
+      if (error.response.status === 500) {
+        console.log("There was a problem with the server");
+      } else {
+        console.log(error.response.data.msg);
+      }
+    }
   };
   const fileInputHandler = (e) => {
-    setSelectedFile(e.target.files[0]);
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
   };
 
   return (
-    <form onSubmit={(e) => submitHandler(e)}>
-      <label htmlFor="img">Select image:</label>
-      <input
-        type="file"
-        id="img"
-        name="img"
-        accept="image/*"
-        onChange={(e) => fileInputHandler(e)}
-      />
-      <input type="submit" />
-    </form>
+    <>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <div className="custom-file mb-4">
+          <input
+            type="file"
+            className="custom-file-input"
+            id="custom-file"
+            onChange={fileInputHandler}
+          />
+          <label className="custom-file-label" htmlFor="custom">
+            {fileName}
+          </label>
+        </div>
+
+        <input
+          type="submit"
+          value="Upload"
+          className="btn btn-primary btn-block mt-4"
+        />
+      </form>
+      {uploadedFile ? (
+        <div className="row mt-5">
+          <div className="col-md-6 m-auto">
+            <h3 className="text-center">{uploadedFile.fileName}</h3>
+            <img src={uploadedFile.filePath} style={{ width: "100%" }} />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
