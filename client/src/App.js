e6c5d9cbc5799aed833ca155bf83bf60a8d1add8
hannanel gershinsky <hannanel100@gmail.com>
2021-01-03 17:33:29 +0200

    added server folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from './components/Form/Form'
+import Form from "./components/Form/Form";
 function App() {
   return (
     <div className="App">
